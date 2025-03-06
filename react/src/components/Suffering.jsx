b4ecba73c3b3963599c34aa780178e3de9e3180b
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-06 14:38:34 -0600

    add: some design in suffering

--- react/src/components/Suffering.jsx
+++ react/src/components/Suffering.jsx
@@ -1,7 +1,8 @@
 export const Suffering = () => {
     return (
-        <div>
-            <h1>Suffering</h1>
+        <div className= "border-2 border-black rounded-md px-16 py-8">
+            <p className= "text-2xl text-gray-800">Suffering</p>
+            <p className= "text-gray-600 italic ml-4">(Dukkha)</p>
         </div>
     )
 }
