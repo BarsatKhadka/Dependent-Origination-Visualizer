1e15d68a90942dc8751e230c82218db30ba99604
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-06 17:54:47 -0600

    add: suffering component middle of the canvas, dynamic sizing with useEffect and eventListener

--- react/src/components/Flow.jsx
+++ react/src/components/Flow.jsx
@@ -1,10 +1,33 @@
+import React , {useEffect} from 'react';
 import { ReactFlow, Controls, Background } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import { Suffering } from './Suffering';
 
 export function Flow() {
+    const [dimensions, setDimensions] = React.useState(
+        {width: window.innerWidth, height: window.innerHeight}
+    );
+    useEffect(() => {
+      const handleResize = () => {
+        setDimensions({width: window.innerWidth, height: window.innerHeight})
+    }
+    
+    window.addEventListener('resize', handleResize);
+    return () => {
+        window.removeEventListener('resize', handleResize);
+    }
+
+    
+  },[])
+
+    const nodeTypes = {suffering: Suffering}
+    
+    const initialNodes = [
+      { id: '1', type: 'suffering', position: { x: dimensions.width / 2 -100, y: dimensions.height / 2 -100 } },
+    ];
   return (
     <div style={{ height: '100%', width: '100%' }}>
-      <ReactFlow>
+      <ReactFlow nodes={initialNodes} nodeTypes={nodeTypes}>
         <Background />
         <Controls />
       </ReactFlow>
