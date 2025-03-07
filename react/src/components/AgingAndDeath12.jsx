d414bfc8381d9aa603ad6349258352736f80d813
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-07 10:52:53 -0600

    add: aging and death box

--- react/src/components/AgingAndDeath12.jsx
+++ react/src/components/AgingAndDeath12.jsx
@@ -1,7 +1,8 @@
 export const AgingAndDeath12 = () => {
     return (
-        <div>
-            <p>Aging and Death</p>
-        </div>
+        <div className="border-2 border-black rounded-md px-12 py-8">
+        <p className="text-2xl text-gray-800">Aging & Death</p>
+        <p className="text-gray-600 italic ml-4">(Bhava)</p>
+    </div>
     )
 }
