bdb7924f6d14a87172822b58ff69583c0714b386
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-07 19:02:26 -0600

    add: basic links complete

--- react/src/components/Birth11.jsx
+++ react/src/components/Birth11.jsx
@@ -1,6 +1,21 @@
+import React from 'react';
+import { Handle, Position } from '@xyflow/react';
+
 export const Birth11 = () => {
     return (
         <div className="border-2 border-black rounded-md lg:px-16 lg:py-8 px-2 py-2">
+             <Handle
+                                type="target"
+                                position={Position.Left}
+                                id="left"
+                                style={{ background: '#555', width: '10px', height: '10px' }}
+                              />
+                        <Handle
+                            type="source"
+                            position={Position.Right}
+                            id="right"
+                            style={{ background: '#555', width: '10px', height: '10px' }}
+                            />
             <p className="lg:text-2xl text-gray-800">Birth</p>
             <p className="text-gray-600 italic lg:ml-4">(Jāti)</p>
         </div>
