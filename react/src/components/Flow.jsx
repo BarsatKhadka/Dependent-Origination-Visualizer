29cffa648c993c49400d8ead876a60242cd8344e
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-07 19:31:42 -0600

    update: seperation of nodeTypes

--- react/src/components/Flow.jsx
+++ react/src/components/Flow.jsx
@@ -1,21 +1,11 @@
 import React, { useEffect } from 'react';
 import { ReactFlow, Controls, Background } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { Suffering } from './Suffering';
-import { Ignorance1 } from './Ignorance1';
-import { VolitionalAct2 } from './VolitionalAct2';
-import { Consciousness3 } from './Consciousness3';
-import { NameAndForm4 } from './NameAndForm4';
-import { SixSenses5 } from './SixSenses5';
-import { Contact6 } from './Contact6';
-import { Feeling7 } from './Feeling7';
-import { Craving8 } from './Craving8';
-import { Clinging9 } from './Clinging9';
-import { Becoming10 } from './Becoming10';
-import { Birth11 } from './Birth11';
-import { AgingAndDeath12 } from './AgingAndDeath12';
+
 import { getInitialNodes } from './data/initialNode';
 import { initialEdges } from './data/initialEdges';
+import { nodeTypes } from './data/nodeTypes';
+
 
 export function Flow() {
     const [dimensions, setDimensions] = React.useState(
@@ -35,21 +25,8 @@ export function Flow() {
         };
     }, []);
 
-    const nodeTypes = {
-        suffering: Suffering,
-        ignorance: Ignorance1,
-        formations: VolitionalAct2,
-        consciousness: Consciousness3,
-        nameAndForm: NameAndForm4,
-        sixSenses: SixSenses5,
-        contact: Contact6,
-        feeling: Feeling7,
-        craving: Craving8,
-        clinging: Clinging9,
-        becoming: Becoming10,
-        birth: Birth11,
-        agingAndDeath: AgingAndDeath12,
-    };
+
+
 
 
     return (
---
+++ react/src/components/data/nodeTypes.jsx
@@ -0,0 +1,29 @@
+import { Suffering } from '../Suffering';
+import { Ignorance1 } from '../Ignorance1';
+import { VolitionalAct2 } from '../VolitionalAct2';
+import { Consciousness3 } from '../Consciousness3';
+import { NameAndForm4 } from '../NameAndForm4';
+import { SixSenses5 } from '../SixSenses5';
+import { Contact6 } from '../Contact6';
+import { Feeling7 } from '../Feeling7';
+import { Craving8 } from '../Craving8';
+import { Clinging9 } from '../Clinging9';
+import { Becoming10 } from '../Becoming10';
+import { Birth11 } from '../Birth11';
+import { AgingAndDeath12 } from '../AgingAndDeath12';
+
+export const nodeTypes = {
+    suffering: Suffering,
+    ignorance: Ignorance1,
+    formations: VolitionalAct2,
+    consciousness: Consciousness3,
+    nameAndForm: NameAndForm4,
+    sixSenses: SixSenses5,
+    contact: Contact6,
+    feeling: Feeling7,
+    craving: Craving8,
+    clinging: Clinging9,
+    becoming: Becoming10,
+    birth: Birth11,
+    agingAndDeath: AgingAndDeath12,
+};
