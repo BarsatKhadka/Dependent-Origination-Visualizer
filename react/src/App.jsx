b0d738b5c18953294a7e911ac8411f6c8540a0d0
Barsat Khadka <khadkabarsat598@gmail.com>
2025-03-08 07:58:51 -0600

    add: Github logo and star count UI

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,12 +2,14 @@ import { Flow } from "./components/Flow";
 import '@xyflow/react/dist/style.css';
 import { NextUIProvider } from "@nextui-org/react"
 import { DarkModeToggle } from "./components/ui/toggle";
+import GitHubIcon from "./components/ui/githubIcon";
 
 
 function App() {
   return (
     <>
     <NextUIProvider>
+    <GitHubIcon/>
     <div className="flex justify-end">
     <DarkModeToggle/>
     </div>
